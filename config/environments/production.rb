b4056ddf9b4b23bc9c8a257cea5afde2df0b56f3
Tasuku Kudo <loweng280@gmail.com>
2019-10-24 14:49:38 +0900

    config setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.initialize_on_precompile=false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -39,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -61,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "myapp_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root  'homes#index'
+ 
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  get     '/login',  to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+
+  get    '/tags/:id',  to: 'tags#show'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :posts, only: [:new, :show, :create, :destroy] do
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
+  end
+  
+  resources :relationships, only: [:create, :destroy]
 end
