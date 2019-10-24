1cb84ec9df134f5f54df2e338693af79b5cd4ce1
Tasuku Kudo <loweng280@gmail.com>
2019-10-24 15:37:31 +0900

    create helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :get_tag
+  include SessionsHelper
+
+  private
+
+    def get_tag
+      @tags = Tag.all
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインが必要です。"
+        redirect_to login_url
+      end
+    end
+
 end
+ 
