a6a9bc0a1e6a29c3aabde0f55ae0fe4b872c6e57
Tasuku Kudo <loweng280@gmail.com>
2019-10-26 18:19:28 +0900

    コメントページのCSSを調整

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,5 @@ class Comment < ApplicationRecord
   validates :body, presence: true
   validates :user_id, presence: true
   validates :post_id, presence: true
+  default_scope -> { order(created_at: :desc) }
 end
