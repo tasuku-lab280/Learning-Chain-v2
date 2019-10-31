2298eee31f643d159c69ab4c0d2d0a19a87f92a7
Tasuku Kudo <loweng280@gmail.com>
2019-10-31 20:05:46 +0900

    AWS setting

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,8 +1,8 @@
 if defined?(AssetSync)
   AssetSync.configure do |config|
     config.fog_provider = 'AWS'
-    config.aws_access_key_id = 'AKIAQWA2M3HQTYGJ5CMY'
-    config.aws_secret_access_key = '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'
+    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
     config.fog_directory = "learning-chain-app"
     config.fog_region = "ap-northeast-1"
     config.gzip_compression = true
