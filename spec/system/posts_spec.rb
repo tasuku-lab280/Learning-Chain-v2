1196e5bdf1641b4b1f8e617162dabede88228cda
Tasuku Kudo <loweng280@gmail.com>
2019-11-16 20:21:22 +0900

    投稿機能のsystem spec

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,39 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :system do
-  describe "user#showにユーザーの投稿一覧を表示" do
 
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com', password: 'password') } 
-    let!(:post_a) { FactoryBot.create(:post, title: 'test-title', content: 'test-content', user: user_a) }
-    
-    describe '投稿フォームに記入' do
-      before do
-        visit "posts/new"
-        fill_in 'タイトル', with: title_name
-        fill_in '勉強内容', with: content_name
-        click_button "勉強記録を投稿する"
-      end
-    end
+  it "ユーザーは新しい投稿を作成する" do
+    user = FactoryBot.create(:user, name: "test",
+                                   email: "testing@example.com", 
+                                   password: "password", 
+                                   password_confirmation: "password")
 
-    describe 'users#show' do
-      it 'user_a が作成した投稿タイトル、ユーザー名が表示される' do
-        visit user_path(id: user_a.id)
-        expect(page).to have_content 'ユーザーA'
-        expect(page).to have_content 'test-title'
-        expect(page).to have_content 'test-material'
-      end
-    end
+    visit root_path
+    click_link "ログイン"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "ログイン"
 
-    describe 'posts#show' do
-      it 'user_a が作成した投稿内容の詳細を表示する' do
-        visit post_path(id: post_a.id)
-        expect(page).to have_content 'ユーザーA'
-        expect(page).to have_content 'test-title'
-        expect(page).to have_content 'test-material'
-        expect(page).to have_content 'test-content'
-      end
-    end
+    expect {
+      click_link "勉強記録を投稿する"
+      fill_in 'タイトル', with: "30分勉強したよー！"
+      fill_in '教材', with: "Railsチュートリアル"
+      fill_in '勉強内容', with: "Rspecの書き方を勉強した！"
+      click_button "勉強記録を投稿する"
 
-
+      expect(page).to have_content "勉強記録が投稿されました！"
+      expect(page).to have_content "30分勉強したよー！"
+      expect(page).to have_content "Railsチュートリアル"
+      expect(page).to have_content "#{user.name}"
+    }.to change(user.posts, :count).by(1)
   end
 end
+
+
